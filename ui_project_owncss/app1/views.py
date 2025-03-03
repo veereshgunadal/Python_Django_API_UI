c3f699b0604b5dfb63438ec1e8131f377f33c956
veereshgunadal <gunadalveeresh@gmail.com>
2025-03-03 05:38:42 -0800

    ui project with owncss

---
+++ ui_project_owncss/app1/views.py
@@ -0,0 +1,72 @@
+from django.shortcuts import render,redirect
+
+from app1.models import Movies
+from app1.forms import MoviesForm
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def add(request):
+    form = MoviesForm
+    return render(request, 'add.html', {'form':form})
+
+def add_result(request):
+    form = MoviesForm(request.POST)
+    if form.is_valid():
+        form.save(commit=True)
+        return render (request, 'addupdatedelete_result.html', {'action':'add movie'})
+    else:
+        return render("not_found")
+    
+def get(request):
+    return render(request, 'get.html')
+
+def get_result(request):
+    try:
+        name = request.GET.get("name")
+        data_obj = Movies.objects.get(name = name)
+        res_data = [data_obj,]
+        return render(request, 'getresult.html', {'movies': res_data})
+    except:
+        return redirect("not_found")
+    
+def delete(request):
+    return render(request, 'delete.html')
+
+def delete_result(request):
+    try:
+        name = request.GET.get("name")
+        data_obj = Movies.objects.get("name")
+        status, data = data_obj.delete()
+        if status == 1:
+            return render(request, 'addupdatedelete_result.html', {'action': 'delete'})
+    except:
+        return redirect("not_found")
+    
+def update(request):
+    return render(request, 'update.html')
+
+def updating(request):
+    try:
+        form = MoviesForm
+        name = request.GET.get("name")
+        data_obj = Movies.objects.get(name)
+        return render(request, 'updating.html', {'form': form, 'moviename':data_obj.name})
+    except Exception as e:
+        print(e)
+        return redirect("not_found")
+    
+def update_result(request, name):
+    try:
+        data_obj = Movies.objects.get(name = name)
+        form = MoviesForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            return render(request, 'addupdatedelete_result.html', {'action': 'update'})
+    except:
+        return redirect("not_found")
+    
+def not_found(request):
+    return render(request, 'notfound.html')
